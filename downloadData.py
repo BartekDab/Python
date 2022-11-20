ed5a91e73fcf6cc969df332b6b9c99892ab8496c
Bartek <bardab@op.pl>
2022-11-20 14:34:42 +0100

    create files

---
+++ downloadData.py
@@ -0,0 +1,12 @@
+from lxml import html #pip install lxml
+import requests #pip install requests
+
+url = "https://www.leroymerlin.pl/zabezpieczenie-domu/systemy-smart-home,a3358.html"
+
+page = requests.get(url)
+tree = html.fromstring(page.content)
+xpath_selector='//*[@id="product-listing"]/div/a/h3/text()'
+products = tree.xpath(xpath_selector)
+for product in products:
+    print(product.strip())
+
