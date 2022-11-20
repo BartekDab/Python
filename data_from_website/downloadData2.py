7c2c5424bd1ff0b23657dd497da270912bb83cbc
Bartek <bardab@op.pl>
2022-11-20 16:59:50 +0100

    download from multiple pages in the same category

--- data_from_website/downloadData2.py
+++ data_from_website/downloadData2.py
@@ -1,12 +1,26 @@
 from lxml import html #pip install lxml
 import requests #pip install requests
+from requests import get
 
-# url = "https://www.leroymerlin.pl/zabezpieczenie-domu/systemy-smart-home,a3358.html"
-url = "https://www.leroymerlin.pl/zabezpieczenie-domu/systemy-smart-home,a3358,strona-2.html"
-page = requests.get(url)
-tree = html.fromstring(page.content)
-xpath_selector='//*[@id="product-listing"]/div/a/h3/text()'
-products = tree.xpath(xpath_selector)
-for product in products:
-    print(product.strip())
+siteNumber = 1
+url = "https://www.leroymerlin.pl/zabezpieczenie-domu/systemy-smart-home,a3358,strona-" + str(siteNumber) +".html"
+# urlCheck = get(url)
+
+while get(url).ok:#urlCheck.ok
+    url = "https://www.leroymerlin.pl/zabezpieczenie-domu/systemy-smart-home,a3358,strona-" + str(siteNumber) +".html"
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    xpath_noResults = '//*[@id="right-content"]/div[2]/text()'
+    noResultsSite = tree.xpath(xpath_noResults)
+    if "W chwili obecnej trwają prace nad zaprezentowaniem w serwisie produktów z wybranej kategorii" in noResultsSite[1]:
+        print("Nie ma strony.")
+        exit()
+    else:
+        xpath_selector='//*[@id="product-listing"]/div/a/h3/text()'
+        products = tree.xpath(xpath_selector)
+        print("\nStrona: " + str(siteNumber))
+        # print(url)
+        for product in products:
+            print(product.strip())
+        siteNumber+=1
 
